89f034d6fb34cf818db7131e58f3e2d61d511334
Heath Hile <95439485+heathhile@users.noreply.github.com>
2025-03-21 22:12:26 -0400

    added report list

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from stats import get_word_count
+from stats import get_word_count, count_chars, make_report
 
 def get_book_text(filepath):
     with open(filepath) as file:
         f_contents = file.read()
     return f_contents
 
-
-
 def main():
     contents = get_book_text("./books/frankenstein.txt")
     num_words = get_word_count(contents)
     print(f"{num_words} words found in the document")
+    num_chars = count_chars(contents.lower())
+    print(f"There are {num_chars} in this document")
+    report = make_report(num_chars, num_words)
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,23 @@
 def get_word_count(fileContents):
     return len(fileContents.split())
+
+def count_chars(the_chars):
+    chars = {}
+    for char in the_chars:
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    return chars    
+
+def make_report(more_chars, words):
+    item_list = list(more_chars.items())
+    sorted_list = sorted(item_list, key=lambda item:item[1], reverse=True)
+    
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{words} words found in the document")
+    print()
+    for char, num in sorted_list:
+        if char.isalpha():
+            print(f"The '{char}' character was found '{num}' times ")
+    print("--- End report ---")
